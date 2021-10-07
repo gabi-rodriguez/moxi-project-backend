a7c5e82c625f279d5010e5919a0d4c1959c94ace
Gabriel Alfonso Rodriguez <gabriel.rodriguez.sj@gmail.com>
2021-10-06 22:04:44 -0300

    Se agrega la conexion a la BD

---
+++ src/main/java/com/grupo116/moxibackend/dao/MedicoDao.java
@@ -0,0 +1,13 @@
+package com.grupo116.moxibackend.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.grupo116.moxibackend.entity.Medico;
+
+public interface MedicoDao extends JpaRepository<Medico, Long> {
+
+	@Query("select m from Medico m where m.id = ?1")
+	public Medico getByIdMedico(Long id);
+
+}
